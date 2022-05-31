ef7fa08c26ff9f6fedf90dfdbc47e33ffe14e2f6
Colin Lappala <colin@amperity.com>
2022-05-31 13:35:37 -0700

    next version snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amperity/wet "0.2.4"
+(defproject amperity/wet "0.2.5-SNAPSHOT"
   :description "Liquid in Clojure"
   :url "https://github.com/amperity/wet"
   :license {:name "Eclipse Public License"
